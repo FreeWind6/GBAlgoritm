569a3f44bafd38738677562c937e6668b452cdf5
Dmitry <free-wind-2012@yandex.ru>
2020-07-11 21:53:10 +0300

    Add hw 6

---
+++ src/main/java/hw6/Main.java
@@ -0,0 +1,38 @@
+package hw6;
+
+import java.util.Random;
+
+/**
+ * 1. Создать и запустить программу для построения двоичного дерева. В цикле построить
+ * двадцать деревьев с глубиной в 6 уровней. Данные, которыми необходимо заполнить узлы деревьев,
+ * представляются в виде чисел типа int. Число, которое попадает в узел, должно генерироваться
+ * случайным образом в диапазоне от -100 до 100.
+ * 2. Проанализировать, какой процент созданных деревьев являются несбалансированными.
+ */
+public class Main {
+    private static final int TREE_COUNT = 20;
+    private static final int MIN_VALUE = -100;
+    private static final int MAX_VALUE = 100;
+    private static final int TREE_HEIGHT = 6;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int bound = MAX_VALUE - MIN_VALUE + 1;
+        int balancedCount = 0;
+
+        for (int i = 0; i < TREE_COUNT; i++) {
+            Tree<Integer> tree = new Tree<>(TREE_HEIGHT);
+            do {
+                try {
+                    tree.add(random.nextInt(bound) + MIN_VALUE);
+                } catch (IndexOutOfBoundsException ex) {
+                    break;
+                }
+            } while (true);
+            if (tree.isBalanced()) {
+                balancedCount++;
+            }
+        }
+        System.out.printf("Сбалансированных деревьев: %d из %d", balancedCount, TREE_COUNT);
+    }
+}
